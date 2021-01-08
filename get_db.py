9260bcc4b624c8967b32edf0e161acc83b13a9bc
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2021-01-08 22:17:46 +0300

    get DB

---
+++ get_db.py
@@ -0,0 +1,25 @@
+import json
+
+from cherche.app import db, Teacher
+from cherche.data import teachers
+
+
+def load_db():
+    for teach in teachers:
+        goals = json.dumps(teach["goals"])
+        free = json.dumps(teach["free"])
+        te = Teacher(
+            name=teach["name"],
+            about=teach["about"],
+            rating=teach["rating"],
+            picture=teach["picture"],
+            price=teach["price"],
+            goals=goals,
+            free=free
+        )
+        db.session.add(te)
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    load_db()
